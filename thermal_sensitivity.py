de6367fc2dcb8d22250170a37664f0d15ab1614e
amounier <andre.mounier@ens-paris-saclay.fr>
2024-08-19 18:48:49 +0200

    add python file for thermal sensitivity analysis

---
+++ thermal_sensitivity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 11:08:40 2024
+
+@author: amounier
+"""
+
+
+import time 
+
+
+
+
+def main():
+    tic = time.time()
+    
+    tac = time.time()
+    print("Done in {:.2f}s.".format(tac-tic))
+    
+if __name__ == '__main__':
+    main()
