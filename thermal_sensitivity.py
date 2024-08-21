c5c2595c3d69e2fe0ec47b3df0d4109291657115
amounier <andre.mounier@ens-paris-saclay.fr>
2024-08-21 14:52:12 +0200

    add method for thermal sensitivity identification

--- thermal_sensitivity.py
+++ thermal_sensitivity.py
@@ -13,8 +13,11 @@ from datetime import date
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+# import tqdm
+from sklearn.metrics import r2_score
+from scipy.optimize import curve_fit
 
-from energy_consumption_RC import get_coordinates, get_meteo_data
+from meteorology import get_meteo_data
 from utils import plot_timeserie
 
 # Définition des dictionnaires administratifs de régions
@@ -85,12 +88,12 @@ def open_electricity_consumption(scale='national', force=False):
         
         if clean_data_file not in os.listdir(enedis_data_folder) or force:
             # Données de consommation aux PDL de moins de 36 kVA (https://www.data.gouv.fr/fr/datasets/agregats-segmentes-de-consommation-electrique-au-pas-1-2-h-des-points-de-soutirage-36kva-maille-nationale-1/)
-            raw_data_file = 'conso-inf36-region.csv'
+            raw_data_file = 'conso-inf36-region_3.csv'
             raw_data = pd.read_csv(os.path.join(enedis_data_folder,raw_data_file), sep=';')
             
             raw_data = raw_data[['horodate', 'profil', 'plage_de_puissance_souscrite','region',
-                                  'nb_points_soutirage', 'total_energie_soutiree_wh']]
-                        
+                                 'nb_points_soutirage', 'total_energie_soutiree_wh']]
+
             # j'ai pas besoin des différences par puissances souscrites
             raw_data = raw_data[raw_data.plage_de_puissance_souscrite=='P0: Total <= 36 kVA']
             
@@ -114,13 +117,16 @@ def open_electricity_consumption(scale='national', force=False):
             data_reg = data[data.region==reg]
             for c in ['nb_points_soutirage', 'total_energie_soutiree_wh']:
                 c_reg = c + '_reg_{}'.format(regcode)
-            reformatted_data[c_reg] = data_reg[c].values
-        
+                reformatted_data[c_reg] = data_reg[c].values
+            
         data = pd.DataFrame().from_dict(reformatted_data)
         data = data.set_index('horodate')
         data.index = pd.to_datetime(data.index)
         data = data.groupby(pd.Grouper(freq='h')).sum()
         data.index = data.index.tz_localize(None)
+        
+        for c in data.columns:
+            data[c] = data[c].replace({0:np.nan})
         
     return data
 
@@ -153,6 +159,7 @@ def main():
         sum_reg = regional_consumption_data.copy()
         sum_reg = sum_reg[[c for c in sum_reg.columns if c.startswith('total_energie_soutiree')]]
         sum_reg = pd.DataFrame(sum_reg.sum(axis=1)).rename(columns={0:'total_energie_soutiree_wh_sum_reg'})
+        sum_reg['total_energie_soutiree_wh_sum_reg'] = sum_reg['total_energie_soutiree_wh_sum_reg'].replace({'0':np.nan, 0:np.nan})
         fig,ax = plot_timeserie(national_consumption_data[['total_energie_soutiree_wh']], figsize=(10,5),
                                 figs_folder=figs_folder, save_fig='total_energie_soutiree_wh_national_enedis',
                                 show=False, alpha=0.5)
@@ -161,18 +168,26 @@ def main():
                                 show=False, alpha=0.5)
         
         
-    # plot_timeserie(national_data[['nb_points_soutirage']], figsize=(10,5),
+    # plot_timeserie(national_consumption_data[['nb_points_soutirage']], figsize=(10,5),
     #                figs_folder=figs_folder, save_fig='nb_points_soutirage_national_enedis')
-    # plot_timeserie(national_data[['total_energie_soutiree_wh']], figsize=(10,5),
+    # plot_timeserie(national_consumption_data[['total_energie_soutiree_wh']], figsize=(10,5),
     #                figs_folder=figs_folder, save_fig='total_energie_soutiree_wh_national_enedis')
     
+    # reg = 93
+    # plot_timeserie(regional_consumption_data[['total_energie_soutiree_wh_reg_{}'.format(reg)]], figsize=(10,5),
+    #                figs_folder=figs_folder, save_fig='total_energie_soutiree_wh_reg{}_enedis'.format(reg))
     
     
-    
-    # Premier test de thermosensibilité
+    # Premiers tests de thermosensibilité
     if True:
-        paris_meteo_data = get_meteo_data('Paris',[2020,2024])
-        data = paris_meteo_data.join(national_consumption_data,how='outer')
+        reg_code = 93#11#93#76#93
+        year = 2022
+        city = dict_region_code_chef_lieu.get(reg_code)
+        reg_name = dict_region_code_region_name.get(reg_code)
+        
+        meteo_data = get_meteo_data(city,[2022,2024])
+        meteo_data = get_meteo_data(city,[year,year])
+        data = meteo_data.join(regional_consumption_data,how='inner')
         
         # data['weekday'] = data.index.weekday
         # weekday_data = dict()
@@ -182,24 +197,46 @@ def main():
         # fig,ax = plt.subplots(figsize=(5,5),dpi=300)
         # ax.errorbar(list(weekday_data.keys()),[np.nanmean(weekday_data.get(i)) for i in range(0,7)], yerr = [np.nanstd(weekday_data.get(i)) for i in range(0,7)])
         
-        def piecewise_linear(x, x0, x1, y0, k1, k2):
-            return np.piecewise(x, [x <= x0, (x > x0)&(x<x1), x>=x1], [lambda x:k1*x + y0-k1*x0, lambda x: y0, lambda x:k2*x + y0-k2*x0])
+        def piecewise_linear(T, Th, Tc, C0, kh, kc):
+            res = np.piecewise(T, [T < Th, (T >= Th)&(T<=Tc), T>Tc], [lambda T: -kh*(T-Th) + C0, lambda T: C0, lambda T: kc*(T-Tc)+C0])
+            return res
         
-        from scipy.optimize import curve_fit
-        
+
         data = data.dropna(axis=0)#[:20000]
-        x = data.temperature_2m.values
-        y = data.total_energie_soutiree_wh.values/1e9
-        p0 = (5, 20, 10, 1,1)
-        p , e = curve_fit(piecewise_linear, x, y, maxfev=100000, p0=p0)
-        xd = np.linspace(-5, 40, 10000)
+        data_temperature_sorted = data.copy().sort_values(by='temperature_2m')
         
-        print(p)
+        x = data_temperature_sorted.temperature_2m.values
+        y = data_temperature_sorted['total_energie_soutiree_wh_reg_{}'.format(reg_code)].values/data_temperature_sorted['nb_points_soutirage_reg_{}'.format(reg_code)].values
+        
+        p0 = (10, 20, 200, 1,1)
+        popt , e = curve_fit(piecewise_linear, x, y, p0=p0)
+        yd = piecewise_linear(x, *popt)
+        r2 = r2_score(y,yd)
+        
+        Th_opt, Tc_opt, C0_opt, kh_opt, kc_opt = popt
+        print(popt)
     
         fig,ax = plt.subplots(figsize=(5,5),dpi=300)
-        ax.plot(x,y,alpha=0.1, ls='',marker='.')
-        ax.plot(xd, piecewise_linear(xd, *p))
+        ax.plot(x,y,alpha=0.1, ls='',marker='.',label='raw_data')
+        label_fit = 'pw linear (R$^2$ = {:.2f})\n   kh=-{:.1f} Wh/K\n   kc={:.2f} Wh/K'.format(r2,kh_opt,kc_opt)
+        ax.plot(x,yd ,label=label_fit)
+        
+        ax.set_ylim(bottom=0.)
+        ylim = ax.get_ylim()
+        
+        ax.plot([Th_opt,Th_opt],ylim,color='k',alpha=0.4)
+        ax.text(Th_opt,10,'{:.1f}°C '.format(Th_opt),horizontalalignment='right',verticalalignment='bottom')
+        ax.plot([Tc_opt,Tc_opt],ylim,color='k',alpha=0.4)
+        ax.text(Tc_opt,10,' {:.1f}°C'.format(Tc_opt),horizontalalignment='left',verticalalignment='bottom')
+        
+        ax.set_ylim(ylim)
+        
+        ax.set_title('{} ({})'.format(reg_name, year))
+        ax.legend(loc='upper right')
+        plt.savefig(os.path.join(figs_folder,'{}.png'.format('thermosensibilite_reg{}_{}'.format(reg_code, year))),bbox_inches='tight')
         plt.show()
+        
+        
         
     tac = time.time()
     print("Done in {:.2f}s.".format(tac-tic))
